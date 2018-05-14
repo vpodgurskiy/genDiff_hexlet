8bd6d5721834e2814344bee6e244733ecb802c37
vpodgurskiy <vpodgurskiy@gmail.com>
2018-05-14 23:16:00 +0300

    add describe with cli

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
+import commander from 'commander';
+import { version, description } from '../../package.json';
 
-console.log('Start gendiff');
+const program = commander;
+
+program
+  .version(`${version}`, '-v, --version')
+  .arguments('<firstConfig> <secondConfig>')
+  .description(`${description}`)
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
