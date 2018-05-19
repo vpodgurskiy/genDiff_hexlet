b1565475db83d3d30378d54e20aec99ecc287711
vpodgurskiy <vpodgurskiy@gmail.com>
2018-05-19 12:53:18 +0300

    concat vs ...spread

--- src/index.js
+++ src/index.js
@@ -24,13 +24,13 @@ const genDiff = (pathFileBefore, pathFileAfter) => {
   const arr = allKeys.reduce((acc, c) => {
     if (_.has(file1, c) && _.has(file2, c)) {
       if (file1[c] === file2[c]) {
-        return acc.concat(`    ${c}: ${file2[c]}`);
+        return [...acc, `    ${c}: ${file2[c]}`];
       }
-      return acc.concat(`  + ${c}: ${file2[c]}`, `  - ${c}: ${file1[c]}`);
+      return [...acc, `  + ${c}: ${file2[c]}`, `  - ${c}: ${file1[c]}`];
     } else if (_.has(file1, c) && !_.has(file2, c)) {
-      return acc.concat(`  - ${c}: ${file1[c]}`);
+      return [...acc, `  - ${c}: ${file1[c]}`];
     } else if (!_.has(file1, c) && _.has(file2, c)) {
-      return acc.concat(`  + ${c}: ${file2[c]}`);
+      return [...acc, `  + ${c}: ${file2[c]}`];
     }
     return acc;
   }, []);
