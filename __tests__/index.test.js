ddc6ebb5225a63348ee6e28fc7027218cfe2b137
vpodgurskiy <vpodgurskiy@gmail.com>
2018-05-20 17:16:46 +0300

    add render JSON

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,9 +4,11 @@ import genDiff from '../src';
 const result = '__tests__/__fixtures__/result.txt';
 const resultOfTree = '__tests__/__fixtures__/resultOfTree.txt';
 const resultOfPlain = '__tests__/__fixtures__/resultOfPlain.txt';
+const resultOfJSON = '__tests__/__fixtures__/resultOfJSON.txt';
 
 const renderTypeTree = 'tree';
 const renderTypePlain = 'plain';
+const renderTypeJSON = 'json';
 
 const pathToFileBeforeJSON = '__tests__/__fixtures__/before.json';
 const pathToFileAfterJSON = '__tests__/__fixtures__/after.json';
@@ -40,6 +42,10 @@ test('genDiff renderPlain for of tree for JSON', () => {
   expect(genDiff(pathToFileBeforeTreeJSON, pathToFileAfterTreeJSON, renderTypePlain)).toBe(fs.readFileSync(resultOfPlain, 'utf8'));
 });
 
+test('genDiff renderJSON for of tree for JSON', () => {
+  expect(genDiff(pathToFileBeforeTreeJSON, pathToFileAfterTreeJSON, renderTypeJSON)).toBe(fs.readFileSync(resultOfJSON, 'utf8'));
+});
+
 const pathToFileBeforeTreeYML = '__tests__/__fixtures__/beforeOfTree.yml';
 const pathToFileAfterTreeYML = '__tests__/__fixtures__/afterOfTree.yml';
 
@@ -51,6 +57,10 @@ test('genDiff renderPlain for of tree for YML', () => {
   expect(genDiff(pathToFileBeforeTreeYML, pathToFileAfterTreeYML, renderTypePlain)).toBe(fs.readFileSync(resultOfPlain, 'utf8'));
 });
 
+test('genDiff renderJSON for of tree for YML', () => {
+  expect(genDiff(pathToFileBeforeTreeYML, pathToFileAfterTreeYML, renderTypeJSON)).toBe(fs.readFileSync(resultOfJSON, 'utf8'));
+});
+
 const pathToFileBeforeTreeINI = '__tests__/__fixtures__/beforeOfTree.ini';
 const pathToFileAfterTreeINI = '__tests__/__fixtures__/afterOfTree.ini';
 
@@ -60,4 +70,8 @@ test('genDiff renderTree for of tree for INI', () => {
 
 test('genDiff renderPlain for of tree for INI', () => {
   expect(genDiff(pathToFileBeforeTreeINI, pathToFileAfterTreeINI, renderTypePlain)).toBe(fs.readFileSync(resultOfPlain, 'utf8'));
+});
+
+test('genDiff renderJSON for of tree for INI', () => {
+  expect(genDiff(pathToFileBeforeTreeINI, pathToFileAfterTreeINI, renderTypeJSON)).toBe(fs.readFileSync(resultOfJSON, 'utf8'));
 });
--- src/renders/index.js
+++ src/renders/index.js
@@ -1,9 +1,11 @@
 import renderOfTree from './renderTree';
 import renderOfPlain from './renderPlain';
+import renderOfJSON from './renderJSON';
 
 const typeOfRenders = {
   tree: renderOfTree,
   plain: renderOfPlain,
+  json: renderOfJSON,
 };
 
 const renderOfData = (ast, type) => {
